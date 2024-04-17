19c277b7a87833ac8ab9edcedf2ab04996e94146
kkvijo <150795457+kkvijo@users.noreply.github.com>
2024-04-17 13:56:32 +0400

    Add files via upload

---
+++ dataframe.py
@@ -0,0 +1,44 @@
+# import pandas as pd
+import pandas as pd
+
+# list of strings
+lst = ['Geeks', 'For', 'Geeks', 'is',
+       'portal', 'for', 'Geeks']
+
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
+
+# Python code demonstrate creating
+# DataFrame from dict narray / lists
+# By default addresses.
+
+
+import pandas as pd
+
+# intialise data of lists.
+data = {'Name': ['Tom', 'nick', 'krish', 'jack'],
+        'Age': [20, 21, 19, 18]}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Print the output.
+print(df)
+
+
+# Import pandas package
+import pandas as pd
+
+# Define a dictionary containing employee data
+data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Age': [27, 24, 22, 32],
+        'Address': ['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+        'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
+
+# select two columns
+print(df[['Name', 'Qualification']])
+
